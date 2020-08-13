8f02b44e3e2842ee0c571a2f5284968c41c27f26
zhangxihe <oncwnuKM9pbHwjiyeE8zuMgBcrPk@git.weixin.qq.com>
2020-08-13 22:03:42 +0800

    feat(tag): 添加tag组件

---
+++ src/components/Tag/index.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Author: 张喜贺
+ * @Date: 2020-08-13 21:38:28
+ * @LastEditors: 张喜贺
+ * @LastEditTime: 2020-08-13 22:01:23
+ * @FilePath: /six-ele/src/components/Tag/index.vue
+-->
+<template>
+  <div class="tag" :style="`color:${color};background:${bgColor}`">
+    <svg-icon v-if="icon" :icon-class="icon" />
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tag",
+  props: {
+    text: String,
+    color: String,
+    bgColor: String,
+    icon: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/var.scss";
+.tag {
+  font-size: 10px;
+  padding: 0 6px;
+  background-color: $gray-3;
+  display: inline-block;
+  border-radius: $border-radius;
+  line-height: 20px;
+  text-align: center;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,21 +2,69 @@
  * @Author: 张喜贺
  * @Date: 2020-08-13 18:48:56
  * @LastEditors: 张喜贺
- * @LastEditTime: 2020-08-13 21:12:17
+ * @LastEditTime: 2020-08-13 22:02:30
  * @FilePath: /six-ele/src/views/home/index.vue
 -->
 <template>
-  <div>
+  <div class="home">
     <search placeholder="请搜索" />
+    <div class="home-tag">
+      <tag
+        v-for="(item, index) in tagList"
+        :key="index"
+        :text="item.text"
+        :color="item.color"
+        :bg-color="item.bgColor"
+        :icon="item.icon"
+      />
+    </div>
+
     home
   </div>
 </template>
 
 <script>
 import Search from "@/components/Search";
+import Tag from "@/components/Tag";
 export default {
-  components: { Search },
+  components: { Search, Tag },
+  data() {
+    return {
+      tagList: [
+        {
+          text: "88金粉节",
+          color: "#FF5028",
+          bgColor: "#FDEFED",
+          icon: "hot",
+        },
+        {
+          text: "麻辣烫",
+        },
+        {
+          text: "麻辣香锅",
+        },
+        {
+          text: "华莱士",
+        },
+        {
+          text: "CoCo",
+        },
+        {
+          text: "奶茶",
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home {
+  &-tag {
+    padding: 0 12px;
+    > * {
+      margin: 0 6px 6px 0;
+    }
+  }
+}
+</style>
