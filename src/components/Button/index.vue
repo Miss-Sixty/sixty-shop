820384eeadbffa31e0edc18bb784d27c18a548c7
zhangxihe <oncwnuKM9pbHwjiyeE8zuMgBcrPk@git.weixin.qq.com>
2020-08-13 21:33:48 +0800

    feat(components): 添加button组件

--- src/components/Button/index.vue
+++ src/components/Button/index.vue
@@ -2,17 +2,38 @@
  * @Author: 张喜贺
  * @Date: 2020-08-13 20:56:14
  * @LastEditors: 张喜贺
- * @LastEditTime: 2020-08-13 20:56:22
+ * @LastEditTime: 2020-08-13 21:33:21
  * @FilePath: /six-ele/src/components/Button/index.vue
 -->
 <template>
-  <div>
-    button
-  </div>
+  <button
+    class="button"
+    :class="{ round: round }"
+    :style="`background:${bgColor};color:${color}`"
+  >
+    {{ text }}
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    round: Boolean,
+    color: String,
+    bgColor: String,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/style/var.scss";
+.button {
+  height: 28px;
+  padding: 0 14px;
+  &.round {
+    border-radius: $border-radius;
+  }
+}
+</style>
--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -2,20 +2,27 @@
  * @Author: 张喜贺
  * @Date: 2020-08-13 20:56:42
  * @LastEditors: 张喜贺
- * @LastEditTime: 2020-08-13 21:20:26
+ * @LastEditTime: 2020-08-13 21:32:25
  * @FilePath: /six-ele/src/components/Search/index.vue
 -->
 <template>
   <div class="search">
     <svg-icon icon-class="search" />
     <input type="text" class="search-input" :placeholder="placeholder" />
-    <button>搜索</button>
+    <Button
+      text="搜索"
+      round
+      bg-color="linear-gradient(to right, #2DC8FE, #02ACF5)"
+      color="#fff"
+    />
   </div>
 </template>
 
 <script>
+import Button from "../Button";
 export default {
   name: "Search",
+  components: { Button },
   props: {
     icon: String,
     placeholder: String,
