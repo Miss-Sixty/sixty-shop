520e218865cd8416fe838663d9719494b78892e1
zhangxihe <oncwnuKM9pbHwjiyeE8zuMgBcrPk@git.weixin.qq.com>
2020-08-14 23:19:37 +0800

    feat(sass): 整理sass

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,14 @@
  * @Author: 张喜贺
  * @Date: 2020-08-13 18:46:48
  * @LastEditors: 张喜贺
- * @LastEditTime: 2020-08-13 20:42:53
+ * @LastEditTime: 2020-08-14 21:57:04
  * @FilePath: /six-ele/src/main.ts
  */
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "@/style/reset.scss";
-import "@/style/hairline.scss";
+import "@/style/index.scss";
 import "@/icons"; // icon
 
 Vue.config.productionTip = false;
