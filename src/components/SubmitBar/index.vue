46bd83b9cc64a38ca57e5e5944796e79368d1147
zhangxihe <oncwnuKM9pbHwjiyeE8zuMgBcrPk@git.weixin.qq.com>
2020-08-18 02:25:54 +0800

    feat(cart): 完成cart ui

---
+++ src/components/SubmitBar/index.vue
@@ -0,0 +1,91 @@
+<!--
+ * @Author: 张喜贺
+ * @Date: 2020-08-18 01:45:18
+ * @LastEditors: 张喜贺
+ * @LastEditTime: 2020-08-18 02:23:26
+ * @FilePath: /six-ele/src/components/SubmitBar/index.vue
+-->
+<template>
+  <div class="submit-bar hairline--top">
+    <p class="select-all">
+      <svg-icon icon-class="passed" size="16" />
+      全选
+    </p>
+
+    <div class="submit-bar__text">
+      <span>合计：</span>
+      <span class="submit-bar__price">
+        ¥<span class="submit-bar__price--integer">30</span>.50
+      </span>
+    </div>
+    <Button text="提交订单" class="submit-bar__button" round />
+  </div>
+</template>
+
+<script>
+import Button from "@/components/Button";
+export default {
+  name: "SubmitBar",
+  components: { Button },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/var.scss";
+.submit-bar {
+  position: fixed;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+  height: $submit-bar-height;
+  z-index: $submit-bar-z-index;
+  background-color: $submit-bar-background-color;
+  padding-bottom: constant(safe-area-inset-bottom); /* 兼容 iOS < 11.2 */
+  padding-bottom: env(safe-area-inset-bottom); /* 兼容 iOS >= 11.2 */
+
+  display: flex;
+  user-select: none;
+  align-items: center;
+  padding: $submit-bar-padding;
+  font-size: $submit-bar-text-font-size;
+
+  .select-all {
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    user-select: none;
+    .svg-icon {
+      margin-right: 5px;
+    }
+  }
+
+  &__text {
+    flex: 1;
+    text-align: right;
+    padding-right: $padding-sm;
+    color: $submit-bar-text-color;
+    span {
+      display: inline-block;
+    }
+  }
+
+  &__price {
+    color: $submit-bar-price-color;
+    font-weight: $font-weight-bold;
+    font-size: $font-size-sm;
+
+    &--integer {
+      font-size: $submit-bar-price-integer-font-size;
+      font-family: $submit-bar-price-font-family;
+    }
+  }
+
+  &__button {
+    width: $submit-bar-button-width;
+    height: $submit-bar-button-height;
+    font-weight: $font-weight-bold;
+    background: $goods-action-button-danger-color;
+    color: $white;
+  }
+}
+</style>
--- src/views/shoppingCart/index.vue
+++ src/views/shoppingCart/index.vue
@@ -2,15 +2,114 @@
  * @Author: 张喜贺
  * @Date: 2020-08-13 18:52:26
  * @LastEditors: 张喜贺
- * @LastEditTime: 2020-08-13 18:53:18
- * @FilePath: /six-ele/src/views/order/index.vue
+ * @LastEditTime: 2020-08-18 02:25:05
+ * @FilePath: /six-ele/src/views/shoppingCart/index.vue
 -->
 <template>
-  <div>order</div>
+  <div class="shopping-cart">
+    <div class="shopping-cart-content">
+      <six-row
+        class="shopping-cart-content-item"
+        type="flex"
+        align="center"
+        v-for="item in 2"
+        :key="item"
+      >
+        <svg-icon icon-class="passed" size="20" />
+        <six-row class="card" type="flex" align="center">
+          <img
+            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597338557818&di=27c3917e22963e3549111637d96ef23f&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F0%2F59929c0367397.jpg"
+            width="80px"
+            height="70px"
+          />
+          <div class="text">
+            <p class="text__title">名字</p>
+            <p class="text__type">规格</p>
+            <six-row
+              class="text__money"
+              type="flex"
+              justify="space-between"
+              align="center"
+            >
+              <p>
+                <span>¥55</span>
+                <span>¥75</span>
+              </p>
+              <div>步进器</div>
+            </six-row>
+          </div>
+        </six-row>
+      </six-row>
+    </div>
+
+    <submit-bar />
+  </div>
 </template>
 
 <script>
-export default {};
+import SixRow from "@/components/Row";
+import SubmitBar from "@/components/SubmitBar";
+export default {
+  name: "ShoppingCart",
+  components: { SixRow, SubmitBar },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/style/var.scss";
+.shopping-cart {
+  padding: $padding-sm;
+  padding-bottom: 100px;
+  background-color: $gray-1;
+  height: 100vh;
+  &-content {
+    box-shadow: 0px 5px 12px rgba(0, 0, 0, 0.16);
+    padding: $padding-xs $padding-sm;
+    border-radius: $border-radius-lg;
+    background-color: $white;
+    &-item {
+      padding: $padding-xs 0;
+
+      .svg-icon {
+        margin-right: 10px;
+      }
+      .card {
+        flex: 1;
+
+        img {
+          border-radius: $border-radius-sm;
+          margin-right: 6px;
+        }
+        .text {
+          flex: 1;
+          &__title {
+            font-size: $font-size-lg;
+            font-weight: $font-weight-bold;
+          }
+          &__type {
+            font-size: $font-size-sm;
+            color: $gray-7;
+          }
+          &__money {
+            margin-top: 6px;
+            p {
+              span {
+                &:first-of-type {
+                  font-size: 15px;
+                  color: $red;
+                  margin-right: 5px;
+                }
+                &:last-of-type {
+                  font-size: $font-size-sm;
+                  color: $gray-6;
+                  text-decoration: line-through;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
