4d444ad874ed157aacd9029b9cf3dc7481a1f633
zhangxihe <oncwnuKM9pbHwjiyeE8zuMgBcrPk@git.weixin.qq.com>
2020-08-18 18:59:44 +0800

    feat(icon): icon组件支持图片链接

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -2,17 +2,19 @@
  * @Author: 张喜贺
  * @Date: 2020-08-13 19:39:17
  * @LastEditors: 张喜贺
- * @LastEditTime: 2020-08-13 20:29:56
+ * @LastEditTime: 2020-08-18 18:44:53
  * @FilePath: /six-ele/src/components/SvgIcon/index.vue
 -->
 <template>
+  <img v-if="isImage" :src="name" :style="imageSize" />
   <svg
+    v-else
     :class="svgClass"
     aria-hidden="true"
     v-on="$listeners"
     :style="{ fontSize: size }"
   >
-    <use :xlink:href="iconName" />
+    <use :xlink:href="`#icon-${this.name}`" />
   </svg>
 </template>
 
@@ -20,32 +22,38 @@
 export default {
   name: "SvgIcon",
   props: {
-    iconClass: {
+    name: {
       type: String,
       required: true,
-    },
-    className: {
-      type: String,
-      default: "",
     },
     size: String,
   },
   computed: {
-    iconName() {
-      return `#icon-${this.iconClass}`;
-    },
     svgClass() {
-      if (this.className) {
-        return "svg-icon " + this.className;
+      if (this.name) {
+        return "svg-icon " + this.name;
       } else {
         return "svg-icon";
       }
+    },
+    isImage() {
+      return this.name ? this.name.indexOf("/") !== -1 : false;
+    },
+    imageSize() {
+      if (this.isImage) {
+        return {
+          width: this.size + "px",
+          height: this.size + "px",
+        };
+      }
+      return null;
     },
   },
 };
 </script>
 
 <style scoped>
+img,
 .svg-icon {
   width: 1em;
   height: 1em;
