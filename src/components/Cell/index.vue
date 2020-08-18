b5c23c307f9877f7409433bb0eda67f454a2f46e
zhangxihe <oncwnuKM9pbHwjiyeE8zuMgBcrPk@git.weixin.qq.com>
2020-08-18 13:13:03 +0800

    feat(cell): 添加cell 组件

---
+++ src/components/Cell/index.vue
@@ -0,0 +1,106 @@
+<!--
+ * @Author: 张喜贺
+ * @Date: 2020-08-18 12:15:22
+ * @LastEditors: 张喜贺
+ * @LastEditTime: 2020-08-18 13:12:31
+ * @FilePath: /six-ele/src/components/Cell/index.vue
+-->
+<template>
+  <div
+    class="cell"
+    :class="clickable ? 'cell--clickable' : ''"
+    @click="$emit('click', $event)"
+  >
+    <svg-icon class="cell__left-icon" v-if="icon" :icon-class="icon" />
+    <div v-if="title" class="cell__title">
+      <span>{{ title }}</span>
+      <div class="cell__label" v-if="label">
+        {{ label }}
+      </div>
+    </div>
+
+    <div class="cell__value" v-if="value">
+      <span>{{ value }}</span>
+    </div>
+
+    <svg-icon class="cell__right-icon" v-if="isLink" icon-class="arrow" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cell",
+  props: {
+    icon: String,
+    title: [Number, String],
+    value: [Number, String],
+    label: [Number, String],
+    isLink: Boolean,
+    clickable: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/var.scss";
+.cell {
+  position: relative;
+  display: flex;
+  width: 100%;
+  padding: $cell-vertical-padding $cell-horizontal-padding;
+  overflow: hidden;
+  color: $cell-text-color;
+  font-size: $cell-font-size;
+  line-height: $cell-line-height;
+  background-color: $cell-background-color;
+
+  &__label {
+    margin-top: $cell-label-margin-top;
+    color: $cell-label-color;
+    font-size: $cell-label-font-size;
+    line-height: $cell-label-line-height;
+  }
+
+  &__title,
+  &__value {
+    flex: 1;
+  }
+
+  &__value {
+    position: relative;
+    overflow: hidden;
+    color: $cell-value-color;
+    text-align: right;
+    vertical-align: middle;
+    word-wrap: break-word;
+  }
+
+  &__left-icon,
+  &__right-icon {
+    min-width: 1em;
+    height: $cell-line-height;
+    font-size: $cell-icon-size;
+    line-height: $cell-line-height;
+  }
+
+  &__left-icon {
+    margin-right: $padding-base;
+  }
+  &__right-icon {
+    margin-left: $padding-base;
+    color: $cell-right-icon-color;
+  }
+
+  &--clickable {
+    cursor: pointer;
+
+    &:active {
+      background-color: $cell-active-color;
+    }
+  }
+
+  &--center {
+    align-items: center;
+  }
+}
+</style>
--- src/views/me/index.vue
+++ src/views/me/index.vue
@@ -2,15 +2,79 @@
  * @Author: 张喜贺
  * @Date: 2020-08-13 18:52:30
  * @LastEditors: 张喜贺
- * @LastEditTime: 2020-08-13 18:53:09
+ * @LastEditTime: 2020-08-18 13:12:19
  * @FilePath: /six-ele/src/views/me/index.vue
 -->
 <template>
-  <div>me</div>
+  <div class="me">
+    <div class="me-login">
+      <img
+        src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3728634894,1931124514&fm=26&gp=0.jpg"
+        alt=""
+        width="50px"
+        height="50px"
+        style="border-radius:50%"
+      />
+      <p class="me-login__text">
+        立即登陆
+        <svg-icon icon-class="success" />
+      </p>
+    </div>
+
+    <div class="me-cell">
+      <cell title="单元格" isLink clickable @click="onClick" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Cell from "@/components/Cell";
+export default {
+  name: "Me",
+  components: { Cell },
+  methods: {
+    onClick(e) {
+      console.log(e);
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/style/var.scss";
+.me {
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  &::after {
+    content: " ";
+    background-color: #fbbd08;
+    height: 120px;
+    width: 150%;
+    border-radius: 0 0 50% 50%;
+    position: absolute;
+    left: -25%;
+    top: 0;
+    z-index: -1;
+  }
+  &-login {
+    display: flex;
+    align-items: center;
+    padding: $me-login-padding;
+    &__text {
+      margin-left: $me-login-text-left;
+      font-size: $me-login-text-font-size;
+      color: $me-login-text-color;
+      font-weight: $font-weight-bold;
+    }
+  }
+
+  &-cell {
+    margin: $me-cell-margin;
+    background-color: $me-cell-background-color;
+    box-shadow: $me-cell-box-shadow;
+    border-radius: $me-cell-border-radius;
+  }
+}
+</style>
